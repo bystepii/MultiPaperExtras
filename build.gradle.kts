e1fb35e1fb55a12df76e03e731b230b079751904
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2024-11-19 11:12:22 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    kotlin("jvm") version "2.1.0-RC2"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "io.multipaper"
+version = "1.0-SNAPSHOT"
+description = "Extra commands for MultiPaper"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/") {
+        name = "papermc-repo"
+    }
+    maven("https://repo.clojars.org/") {
+        name = "clojars"
+    }
+    maven("https://oss.sonatype.org/content/groups/public/") {
+        name = "sonatype"
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.github.puregero:multilib:1.2.4")
+}
+
+val targetJavaVersion = 17
+kotlin {
+    jvmToolchain(targetJavaVersion)
+}
+
+tasks.build {
+    dependsOn("shadowJar")
+}
+
+tasks.processResources {
+    val props = mapOf(
+        "version" to version,
+        "name" to project.name,
+        "description" to description
+    )
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("**/plugin.yml") {
+        expand(props)
+    }
+}
---
+++ src/main/kotlin/io/multipaper/extras/MultiPaperExtras.kt
@@ -0,0 +1,14 @@
+package io.multipaper.extras
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MultiPaperExtras : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
