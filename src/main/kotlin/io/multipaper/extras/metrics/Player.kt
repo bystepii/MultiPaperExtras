1dfbce856a9e58048be660a4a042afc12dec4752
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2024-11-27 15:45:21 +0100

    Implement player location metrics

---
+++ src/main/kotlin/io/multipaper/extras/metrics/Player.kt
@@ -0,0 +1,50 @@
+package io.multipaper.extras.metrics
+
+import dev.cubxity.plugins.metrics.api.metric.collector.Collector
+import dev.cubxity.plugins.metrics.api.metric.collector.CollectorCollection
+import dev.cubxity.plugins.metrics.api.metric.data.GaugeMetric
+import dev.cubxity.plugins.metrics.api.metric.data.Metric
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+import org.bukkit.plugin.Plugin
+
+class Player(plugin: Plugin) : CollectorCollection {
+    override val collectors = listOf(PlayersOnlineLocalCollector(), PlayerLocationCollector())
+    private val logger = plugin.logger
+
+    class PlayersOnlineLocalCollector : Collector {
+        override fun collect(): List<Metric> {
+            val metric = mutableListOf<Metric>()
+            for (world in Bukkit.getWorlds()) {
+                val players = world.players.stream().filter(Player::isLocalPlayer).count()
+                metric.add(GaugeMetric("mc_players_online_local", mapOf("world" to world.name), players))
+            }
+            return metric
+        }
+    }
+
+    inner class PlayerLocationCollector : Collector {
+        private val serverName = Bukkit.getLocalServerName()
+        override fun collect(): List<Metric> {
+            val metric = mutableListOf<Metric>()
+            for (player in Bukkit.getLocalOnlinePlayers()) {
+                val chunk = player.location.chunk
+                var chunkOwner = ""
+                if (chunk.isExternalChunk)
+                    chunkOwner = chunk.externalServerName
+                else if (chunk.isLocalChunk)
+                    chunkOwner = serverName
+                else
+                    logger.warning("Chunk is not local or external: $chunk")
+                metric.add(GaugeMetric("mc_player_location", mapOf(
+                    "name" to player.name,
+                    "world" to player.world.name,
+                    "chunk_x" to "${chunk.x}",
+                    "chunk_z" to "${chunk.z}",
+                    "chunk_owner" to chunkOwner
+                ), 1))
+            }
+            return metric
+        }
+    }
+}
